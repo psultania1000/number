e37d626926beee3e13397713a8e2bea26378249b
Priyansh Sultania <psultania1000@gmail.com>
2025-03-22 02:11:36 +0530

    Initial setup

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,22 @@
+import { TextInput, View, StyleSheet } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+
+function StartGameScreen() {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput />
+      <PrimaryButton>Reset!</PrimaryButton>
+      <PrimaryButton>Concdcdsfirm</PrimaryButton>
+    </View>
+  );
+}
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    padding: 16,
+    marginTop: 100,
+  },
+});
